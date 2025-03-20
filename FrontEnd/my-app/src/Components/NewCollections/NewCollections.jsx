d77ff46c6b34e34f1f3abf11411989c099874e48
Gulam <alghouse@eand.com>
2025-03-20 15:32:09 +0530

    3rd Commit

--- FrontEnd/my-app/src/Components/NewCollections/NewCollections.jsx
+++ FrontEnd/my-app/src/Components/NewCollections/NewCollections.jsx
@@ -9,7 +9,7 @@ const NewCollections = () => {
     const [new_collection, setNew_Collection] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:4000/newcollections')
+        fetch('https://gulam-shop-backend.onrender.com/newcollections')
             .then((response) => response.json())
             .then((data) => setNew_Collection(data));
     }, [])
